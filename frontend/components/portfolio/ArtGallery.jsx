c232cdae833724dc4523e4d45b41c67ef03335ce
Korwin Briggs <Korwin.Briggs@gmail.com>
2022-07-07 17:19:12 -0400

    adding masonry to gallery

--- frontend/components/portfolio/ArtGallery.jsx
+++ frontend/components/portfolio/ArtGallery.jsx
@@ -1,18 +1,29 @@
 
-
+import Masonry from 'react-masonry-css';
 import Painting from './Painting';
 
 
 const Gallery = (props) => {
 
     const {backurl, paintings} = props;
+    const galleryBreakpoints = {
+        default: 3,
+        1100: 2,
+        600: 1,
+    };
 
     return (
         <div className="painting-gallery">
-            {paintings.map( painting => (
-                <Painting backurl={backurl} data={painting.attributes}/>
-                //note to self: have to pass props down like this; can't import directly to component
-            ))}
+
+            <Masonry breakpointCols={galleryBreakpoints} className='painting-gallery-grid' columnClassName='painting-gallery-column'>
+
+                {paintings.map( painting => (
+                    <Painting backurl={backurl} data={painting.attributes}/>
+                    //note to self: have to pass props down like this; can't import directly to component
+                ))}
+
+            </Masonry>
+
         </div>
     );
 }
